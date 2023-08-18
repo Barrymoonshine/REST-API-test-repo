aa1e426022d1e8577d05fbdf54929d67d8e50087
Nick Sheward <nickshwrd@gmail.com>
2023-08-18 11:14:54 +0100

    Added RESTful post and get requests

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+const users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+};
+
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
 
 // Set up Express app
 const app = express();
@@ -8,19 +33,30 @@ app.listen(3000);
 
 // Middleware & static files
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Register View Engine (EJS)
 app.set('view engine', 'ejs');
 
-// Render a view
-app.get('/users', (req, res) => res.send('GET HTTP method on user resource'));
+app.get('/users', (req, res) => res.send(Object.values(users)));
 
-app.post('/users', (req, res) => res.send('POST HTTP method on user resource'));
+app.get('/users/:userId', (req, res) => res.send(users[req.params.userId]));
 
-app.put('/users/:userId', (req, res) =>
-  res.send(`PUT HTTP method on user/${req.params.userId} resource`)
+app.get('/messages', (req, res) => res.send(Object.values(messages)));
+
+app.get('/messages/:messageId', (req, res) =>
+  res.send(messages[req.params.messageId])
 );
 
-app.delete('/users/:userId', (req, res) =>
-  res.send(`DELETE HTTP method on user/${req.params.userId} resource`)
-);
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
